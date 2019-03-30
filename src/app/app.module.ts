1f9a46a890e28d50a3e6828567e37eeef5e450e3
CaritoDasilvagit <caritodasilva@gmail.com>
2019-03-30 09:12:12 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import your library
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Specify the ngx-auth-firebaseui library as an import
+    NgxAuthFirebaseUIModule.forRoot({
+      apiKey: 'AIzaSyBM7Ue0pHRYImLt0v0JYjLmr2oQAswDEts',
+      authDomain: 'girljob.firebaseapp.com',
+      databaseURL: 'https://girljob.firebaseio.com',
+      projectId: 'girljob',
+      storageBucket: 'girljob.appspot.com',
+      messagingSenderId: '1056645454793'
+    }),
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
